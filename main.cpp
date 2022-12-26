8dffcf3ab4cb1487c4bc20d1a4ac0785ce0a707b
balk0n <dvbadyagin@edu.hse.ru>
2022-12-26 12:20:21 +0300

    Задача 1

--- main.cpp
+++ main.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "main.h"
+
+using namespace std;
+
+
+//Меняет местами значения 2-х переменных
+template <typename T>
+void swapp(T *ptr_a, T *ptr_b){
+    T *ptr_c = new T;
+    *ptr_c = *ptr_b;
+    *ptr_b = *ptr_a;
+    *ptr_a = *ptr_c;
+    delete ptr_c;
+}
+
+int main(){
+
+    //Задача 1
+    int a = 5;
+    int b = 6;
+    swapp(&a, &b);
+    cout << a << " " << b << endl;
+
+
+    return 0;
+}
--- main.h
+++ main.h
@@ -2,7 +2,11 @@
 // Created by balk0n on 26.12.2022.
 //
 
+#include <iostream>
 #ifndef ARRAYSHW_MAIN_H
 #define ARRAYSHW_MAIN_H
 
+void swap(int a, int b);
+
 #endif //ARRAYSHW_MAIN_H
+
